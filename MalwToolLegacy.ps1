84c2319bc107093ee3a797810162517afd8d3bc1
ImMALWARE <notmalware@inbox.ru>
2025-02-23 23:23:34 +0300

    ~

--- MalwToolLegacy.ps1
+++ MalwToolLegacy.ps1
@@ -3,9 +3,9 @@ $n = [Environment]::NewLine
 $tls = "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12"
 $host.ui.RawUI.WindowTitle = 'MalwTool Legacy'
 if ($PSUICulture -eq "ru-RU") {
-    $string = "Введите цифру нужного пункта и нажмите Enter:$n 1. Активировать Windows 8/8.1$n 2. Активировать Office 2016$n 3. Активировать Office 2013$n 4. Выход"
+    $strings = @("Введите цифру нужного пункта и нажмите Enter:$n 1. Активировать Windows 8/8.1$n 2. Активировать Office 2016$n 3. Активировать Office 2013$n 4. Выход", "%p% не найден!")
 } else {
-    $string = "Enter the number of what you need and press Enter:$n 1. Activate Windows 8/8.1$n 2. Activate Office 2016$n 3. Activate Office 2013$n 4. Exit"
+    $strings = @("Enter the number of what you need and press Enter:$n 1. Activate Windows 8/8.1$n 2. Activate Office 2016$n 3. Activate Office 2013$n 4. Exit", "%p% not found!")
 }
 
 while ($true) {
@@ -16,9 +16,14 @@ while ($true) {
         "1" {
             Start-Process powershell -ArgumentList "$tls; irm https://raw.githubusercontent.com/ImMALWARE/MABeta/main/Activators/KMS.ps1 | iex" -Verb RunAs
         } "2" {
+            # if (test-path "")
             Start-Process powershell -ArgumentList "`$Product = 'Office2016'; $tls; irm https://raw.githubusercontent.com/ImMALWARE/MABeta/main/Activators/Osppcs.ps1 | iex" -Verb RunAs
         } "3" {
-            Start-Process powershell -ArgumentList "$tls; irm https://raw.githubusercontent.com/ImMALWARE/MABeta/main/Activators/Osppcs2013.ps1 | iex" -Verb RunAs
+            if (test-path "$env:ProgramFiles\Microsoft Office\Office15") {
+                Start-Process powershell -ArgumentList "$tls; irm https://raw.githubusercontent.com/ImMALWARE/MABeta/main/Activators/Osppcs2013.ps1 | iex" -Verb RunAs
+            } else {
+                Write-Host $strings[1].Replace('%p%', 'Office 2016')
+            }
         } "4" {
             exit
         } default {
